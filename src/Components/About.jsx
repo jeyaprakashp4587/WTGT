65d0332b6b93a5de9bae64d1966e170db9d0d788
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-11-25 09:57:45 +0530

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,17 +2,31 @@
 
 import React from "react";
 import "./about.css";
+import aboutImg from "../assets/about.jpg";
 
 const About = () => {
   return (
     <div className="about-wrapper container mt-5" id="about">
       <div className="about-wrapper-left">
-        <img src="https://i.ibb.co/XDqtvsJ/5155720-2672335.jpg" alt="" />
+        <img src={aboutImg} alt="" />
       </div>
       <div className="about-wrapper-right">
         <h2 className="text">Who we are?</h2>
-        <p>We are an Start up specialized in developing Web applications and Mobile applications with <span>Latest Technologies & Love.</span></p>
-        <p>It's okay if you already developed the website, We also <span>re-design and maintenance</span> your website with current trend and standard.</p>
+        <p>
+          At WTGT, we specialize in innovative web app development to transform
+          your ideas into functional, engaging, and user-friendly digital
+          solutions. Our mission is to empower businesses and individuals by
+          creating cutting-edge web applications that combine modern design,
+          robust functionality, and seamless performance. We believe in
+          delivering excellence through every line of code. Whether you're a
+          startup looking to establish your online presence or an enterprise
+          aiming to optimize workflows, WTGT is your trusted partner in crafting
+          tailored solutions that drive success. Why Choose WTGT? Expertise in
+          the latest web technologies and frameworks. Commitment to delivering
+          scalable and secure applications. Customer-centric approach to ensure
+          your vision comes to life. Let’s turn your web app ideas into
+          reality—where tech gets transformed!
+        </p>
       </div>
     </div>
   );
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -2,31 +2,64 @@
 
 import React from "react";
 import "./work.css";
+import webDevelopImg from "../assets/wegDevelopment.jpg";
+import appDevelopImg from "../assets/appDevelopment.png";
+
+import htmlImg from "../assets/html.png";
+import cssImg from "../assets/css-3.png";
+import bootImg from "../assets/bt.png";
+import rnImg from "../assets/rn.png";
+import jslImg from "../assets/js.png";
+import swiftImg from "../assets/sw.png";
+import kotlin from "../assets/kt.png";
 
 const Work = () => {
+  const webTools = [htmlImg, cssImg, jslImg, bootImg, rnImg];
+  const appTools = [rnImg, kotlin, swiftImg];
   return (
     <div className="work-wrapper container" id="work">
       <h2>What We Make?</h2>
       <div className="work-wrappers mt-5 ">
-        <div>
-          <img src="https://i.ibb.co/sJTyDRM/Open-source-rafiki.png" alt="" />
-          <h3>Web Development</h3>
-          <p>
-            We develop websites and webapps with current industry standards.
-          </p>
-          <div className="line1"></div>
+        <div className="webDevelop">
+          <div>
+            <img src={webDevelopImg} alt="" />
+          </div>
+          <div>
+            <h3>Web Development</h3>
+            <p>
+              We develop high-performance websites and web applications tailored
+              to meet your business needs. Our solutions ensure responsiveness,
+              cross-browser compatibility, and adherence to the latest industry
+              standards. From dynamic web apps to static websites, we focus on
+              delivering seamless user experiences and scalable architectures.
+            </p>
+            <div className="Tools">
+              {webTools.map((img) => (
+                <img src={img} />
+              ))}
+            </div>
+          </div>
         </div>
-        <div>
-          <img
-            src="https://i.ibb.co/h1xmhtB/Mobile-development-rafiki.png"
-            alt=""
-          />
-          <h3>App Development</h3>
-          <p>
-            We develop mobile applications where the users can use the
-            applications on both ios and android.
-          </p>
-          <div className="line2"></div>
+        <div className="appDevelop">
+          <div>
+            <img src={appDevelopImg} alt="" />
+          </div>
+          <div>
+            <h3>App Development</h3>
+            <p>
+              Our mobile applications are designed to work flawlessly across
+              both iOS and Android platforms. With a focus on intuitive design,
+              smooth navigation, and robust functionality, we create apps that
+              enhance user engagement and meet the demands of the modern
+              mobile-first world. Whether it's for startups or enterprises, we
+              deliver apps that users love.
+            </p>
+            <div className="Tools">
+              {appTools.map((img) => (
+                <img src={img} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
